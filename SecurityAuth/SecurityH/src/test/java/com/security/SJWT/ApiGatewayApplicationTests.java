eeafb1f2ace63bb1474b9f368a2730ab94d4e095
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2025-08-21 05:15:06 +0530

    Add files via upload

---
+++ SecurityAuth/SecurityH/src/test/java/com/security/SJWT/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.SJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuthApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
